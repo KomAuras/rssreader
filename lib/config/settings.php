1c69c3241a18c90d113d5ee49b4cfab685b35063
Evgeny Stefanenko <mixer@clarionlife.net>
2017-09-13 20:29:33 +1000

    fix again

--- lib/config/settings.php
+++ lib/config/settings.php
@@ -3,7 +3,7 @@
 return array(
     'state' => array('value' => 0),
     'def_feed' => array('value' => 1),
-    'feed_url' => array('value' => 'http://feeds.feedburner.com/webasystcom/ru'),
+    'feed_url' => array('value' => /*_wp*/('http://feeds.feedburner.com/webasystcom')),
     'feed_posts' => array('value' => 5),
 );
 
